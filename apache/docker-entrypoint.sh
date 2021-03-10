a03b72a9d88927f7ac99f28aaa9fe202359ea8cb
Kite <ixkaito@gmail.com>
2021-03-10 18:42:14 +0900

    Testing entrypoint

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -1,3 +1,14 @@
-#!/bin/bash -e
+# !/bin/bash
 
-echo "hello"
+set -m
+
+# /usr/bin/mysqld_safe &
+
+# source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND &
+
+# /usr/local/bin/mailhog
+
+
+/usr/bin/supervisord
+
+fg %1
