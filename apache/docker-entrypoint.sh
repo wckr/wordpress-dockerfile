bd81f25f1f77d563f18b5197b562d15a8382dbc6
Kite <ixkaito@gmail.com>
2021-03-11 22:29:15 +0900

    Improve entrypoint

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -e index.php ] && [ ! -e wp-includes/version.php ] && [ -e /var/www/wordpress.tar.gz ]; then
+  tar -zxf /var/www/wordpress.tar.gz -C /var/www
+fi
+
+if [ -e /var/www/wordpress.tar.gz ]; then
+  rm /var/www/wordpress.tar.gz
+fi
+
 /usr/bin/mysqld_safe &
 
 /usr/local/bin/mailhog &
